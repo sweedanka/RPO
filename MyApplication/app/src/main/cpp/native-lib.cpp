2dcccc7b9a391ede0bfb233914d8bc3a56300017
Danil <dankazavr11@yandex.ru>
2023-03-17 14:05:24 +0300

    2 and 3 labs

--- MyApplication/app/src/main/cpp/native-lib.cpp
+++ MyApplication/app/src/main/cpp/native-lib.cpp
@@ -84,39 +84,125 @@ Java_com_example_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray key, jbyte
     return dout;
 }
 
-
-extern "C" JNIEXPORT jbyteArray JNICALL
-Java_com_example_MainActivity_decrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data)
-{
-    jsize ksz = env->GetArrayLength(key);
-    jsize dsz = env->GetArrayLength(data);
-    if ((ksz != 16) || (dsz <= 0) || ((dsz % 8) != 0)) {
-        return env->NewByteArray(0);
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_example_MainActivity_transaction(JNIEnv env, jobject thiz, jbyteArray trd) {
+    jclass cls = env->GetObjectClass(thiz);
+    jmethodID id = env->GetMethodID(cls, "enterPin", "(ILjava/lang/String;)Ljava/lang/String;");
+    //TRD 9F0206000000000100 = amount = 1р
+    uint8_t* p = (uint8_t*)env->GetByteArrayElements (trd, 0);
+    jsize sz = env->GetArrayLength (trd);
+    if ((sz != 9) || (p[0] != 0x9F) || (p[1] != 0x02) || (p[2] != 0x06))
+        return false;
+    char buf[13];
+    for (int i = 0; i < 6; i++) {
+        uint8_t n = *(p + 3 + i);
+        buf[i*2] = ((n & 0xF0) >> 4) + '0';
+        buf[i*2 + 1] = (n & 0x0F) + '0';
     }
-    mbedtls_des3_context ctx;
-    mbedtls_des3_init(&ctx);
+    buf[12] = 0x00;
+    jstring jamount = (jstring) env->NewStringUTF(buf);
+    int ptc = 3;
+    while (ptc > 0) {
 
-    jbyte * pkey = env->GetByteArrayElements(key, 0);
+        jstring pin = (jstring) env->CallObjectMethod(thiz, id, ptc, jamount);
+        const char * utf = env->GetStringUTFChars(pin, nullptr);
+        env->ReleaseStringUTFChars(pin, utf);
+        if ((utf != nullptr) && (strcmp(utf, "1234") == 0))
+            break;
+        ptc--;
+    }
 
-    uint8_t * buf = new uint8_t[dsz];
-
-    jbyte * pdata = env->GetByteArrayElements(data, 0);
-    std::copy(pdata, pdata + dsz, buf);
-    mbedtls_des3_set2key_dec(&ctx, (uint8_t *)pkey);
-    int cn = dsz / 8;
-    for (int i = 0; i < cn; i++)
-        mbedtls_des3_crypt_ecb(&ctx, buf + i*8, buf +i*8);
-
-    //PKCS#5. упрощено. по соображениям безопасности надо проверить каждый байт паддинга
-    int sz = dsz - 8 + buf[dsz-1];
-
-    jbyteArray dout = env->NewByteArray(sz);
-    env->SetByteArrayRegion(dout, 0, sz, (jbyte *)buf);
-    delete[] buf;
-    env->ReleaseByteArrayElements(key, pkey, 0);
-    env->ReleaseByteArrayElements(data, pdata, 0);
-    return dout;
+    env->ReleaseByteArrayElements(trd, (jbyte *)p, 0);
+    return (ptc > 0);
 }
+
+
+JavaVM* gJvm = nullptr;
+
+JNIEXPORT jint JNICALL JNI_OnLoad (JavaVM* pjvm, void* reserved)
+{
+    gJvm = pjvm;
+    return JNI_VERSION_1_6;
+}
+
+
+JNIEnv* getEnv (bool& detach)
+{
+    JNIEnv* env = nullptr;
+    int status = gJvm->GetEnv ((void**)&env, JNI_VERSION_1_6);
+    detach = false;
+    if (status == JNI_EDETACHED)
+    {
+        status = gJvm->AttachCurrentThread (&env, NULL);
+        if (status < 0)
+        {
+            return nullptr;
+        }
+        detach = true;
+    }
+    return env;
+}
+
+
+void releaseEnv (bool detach, JNIEnv* env)
+{
+    if (detach && (gJvm != nullptr))
+    {
+        gJvm->DetachCurrentThread ();
+    }
+}
+
+JNIEXPORT jboolean JNICALL
+Java_com_example_rpolab_MainActivity_transaction(JNIEnv *xenv, jobject xthiz, jbyteArray xtrd) {
+    jobject thiz = xenv->NewGlobalRef(xthiz);
+    jbyteArray trd = (jbyteArray) xenv->NewGlobalRef(xtrd);
+
+    std::thread t([thiz, trd] {
+        bool detach = false;
+        JNIEnv *env = getEnv(detach);
+        jclass cls = env->GetObjectClass(thiz);
+        jmethodID id = env->GetMethodID(cls, "enterPin", "(ILjava/lang/String;)Ljava/lang/String;");
+        //TRD 9F0206000000000100 = amount = 1р
+        uint8_t *p = (uint8_t *) env->GetByteArrayElements(trd, 0);
+        jsize sz = env->GetArrayLength(trd);
+        if ((sz != 9) || (p[0] != 0x9F) || (p[1] != 0x02) || (p[2] != 0x06))
+            return false;
+
+        char buf[13];
+        for (int i = 0; i < 6; i++) {
+            uint8_t n = *(p + 3 + i);
+            buf[i * 2] = ((n & 0xF0) >> 4) + '0';
+            buf[i * 2 + 1] = (n & 0x0F) + '0';
+        }
+        buf[12] = 0x00;
+        jstring jamount = (jstring) env->NewStringUTF(buf);
+
+        int ptc = 3;
+        while (ptc > 0) {
+            jstring pin = (jstring) env->CallObjectMethod(thiz, id, ptc, jamount);
+            const char *utf = env->GetStringUTFChars(pin, nullptr);
+            env->ReleaseStringUTFChars(pin, utf);
+            if ((utf != nullptr) && (strcmp(utf, "1234") == 0))
+                break;
+            ptc--;
+        }
+
+        id = env->GetMethodID(cls, "transactionResult", "(Z)V");
+        env->CallVoidMethod(thiz, id, ptc > 0);
+        env->ReleaseByteArrayElements(trd, (jbyte *) p, 0);
+        env->DeleteGlobalRef(thiz);
+        env->DeleteGlobalRef(trd);
+        releaseEnv(detach, env);
+        return true;
+    });
+
+    t.detach();
+    return true;
+}
+
+
+
 
 
 
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,73 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
+
+interface TransactionEvents {
+    String enterPin(int ptc, String amount);
+    void transactionResult(boolean result);
+}
+
+public class MainActivity extends AppCompatActivity implements TransactionEvents{
+
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private String pin;
+
+    public native boolean transaction(byte[] trd);
+
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
+    }
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
-
     }
+
+    ActivityResultLauncher activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +76,44 @@ public class MainActivity extends AppCompatActivity {
         int res =initRng();
         byte[] v = randomBytes(10);
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        /*TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());*/
+
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback() {
+                    @Override
+                    public void onActivityResult(Object result) {
+                    }
+                    //@Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+//                            String pin = data.getStringExtra("pin");
+//                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
+                        }
+                    }
+                });
+
+        TextView ta = findViewById(R.id.txtAmount);
+        String amt = String.valueOf(getIntent().getStringExtra("amount"));
+        Long f = Long.valueOf(amt);
+        DecimalFormat df = new DecimalFormat("#,###,###,##0.00");
+        String s = df.format(f);
+        ta.setText("Сумма: " + s);
+
+        TextView tp = findViewById(R.id.txtPtc);
+        int pts = getIntent().getIntExtra("ptc", 0);
+        if (pts == 2)
+            tp.setText("Осталось две попытки");
+        else if (pts == 1)
+            tp.setText("Осталась одна попытка");
+
     }
 
     /**
@@ -34,4 +123,50 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
+
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+
+
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+
+        new Thread(()-> {
+            try {
+                byte[] trd = stringToHex("9F0206000000000100");
+                boolean ok = transaction(trd);
+                runOnUiThread(()-> {
+                    Toast.makeText(MainActivity.this, ok ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+                });
+
+            } catch (Exception ex) {
+                // todo: log error
+            }
+        }).start();
+        byte[] trd = stringToHex("9F0206000000000100");
+        transaction(trd);
+    }
+
+
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
 }
+
